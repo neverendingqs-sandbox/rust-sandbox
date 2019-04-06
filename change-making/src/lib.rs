08ba1c37e0601cb1d034ace9a655e415e198833f
Mark Tse <mark.tse@neverendingqs.com>
2019-04-06 17:15:27 -0400

    change-making - implementation (snapshot).

--- change-making/src/lib.rs
+++ change-making/src/lib.rs
@@ -1,26 +1,37 @@
-pub struct ChangeMachine<'a> {
-    denominations: &'a [u128],
+use std::collections::HashSet;
+
+pub struct ChangeMachine {
+    denominations: HashSet<usize>,
 }
 
-impl<'a> ChangeMachine<'a> {
-    pub fn new(denominations: &[u128]) -> ChangeMachine {
+impl ChangeMachine {
+    pub fn new(denominations: HashSet<usize>) -> ChangeMachine {
         ChangeMachine{ denominations }
     }
 
-    pub fn get_change(&self, amount: u128) -> &[u128] {
-        return self.denominations;
+    pub fn get_num_coins(&self, amount: usize) -> u128 {
+        let mut subproblems: Vec<u128> = Vec::new();
+        subproblems.push(0);
+
+        for i in 1..(amount + 1) {
+            let mut potentials = Vec::new();
+            
+            for d in &self.denominations {
+                if d <= &i {
+                    let potential = subproblems[i - d] + 1;
+                    potentials.push(potential);
+                }
+            }
+
+            subproblems.push(
+                match potentials.iter().min() {
+                    Some(p) => *p,
+                    None => 0
+                }
+            );
+        }
+
+        *subproblems.last().unwrap()
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_returns_denominations() {
-        let denominations = [1, 2, 3];
-        let sut = ChangeMachine::new(&denominations);
-
-        assert_eq!(denominations, sut.get_change(0));
-    }
-}
--- change-making/tests/lib.rs
+++ change-making/tests/lib.rs
@@ -1,9 +1,28 @@
+use std::collections::HashSet;
+
 use change_making;
+
+fn create_denominations(coins: Vec<usize>) -> HashSet<usize> {
+    let mut denominations: HashSet<usize> = HashSet::new();
+    for c in coins {
+        denominations.insert(c);
+    }
+
+    denominations
+}
 
 #[test]
 fn test_returns_denominations() {
-    let denominations = [1, 2, 3];
-    let sut = change_making::ChangeMachine::new(&denominations);
+    let denominations = create_denominations(vec![1, 5, 10, 25, 100]);
+    let sut = change_making::ChangeMachine::new(denominations);
 
-    assert_eq!(denominations, sut.get_change(0));
+    assert_eq!(7, sut.get_num_coins(124));
+}
+
+#[test]
+fn amountiszero_returns_zero() {
+    let denominations = create_denominations(vec![1, 5, 10, 25, 100]);
+    let sut = change_making::ChangeMachine::new(denominations);
+
+    assert_eq!(0, sut.get_num_coins(0));
 }
